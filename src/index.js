15a76da2e7902b6c4f5a74e7dfeab68b4df80ad5
Asif Alam <alamtheboss232@gmail.com>
2022-08-17 11:12:47 +0100

    Add basic react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// function FavoriteColor () {
+//     const [color, setColor] = useState("red");
+//     return (
+//         <>
+//         <h1>My favourite color is {color}!</h1>
+//         <button type="button" onClick={() => setColor("blue")}>Blue</button>
+//     </>
+//     );
+// }
+
+function Button(props) {
+    const handleClick = () => props.onClickFunction(props.increment);
+    return (
+        <button onClick={handleClick}>
+            +{props.increment}
+        </button>
+    );
+}
+
+function Display (props) {
+    return (
+        <div>{props.message}</div>
+    );
+}
+
+function App () {
+    const [counter, setCounter] = useState(0);
+    const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
+    return (
+        <>
+            <Button onClickFunction={incrementCounter} increment={1}/>
+            <Button onClickFunction={incrementCounter} increment={5}/>
+            <Button onClickFunction={incrementCounter} increment={10}/>
+            <Button onClickFunction={incrementCounter} increment={100}/>
+            <Display message={counter}/>
+        </>
+    );
+
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <>
+          <App />
+      </>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
